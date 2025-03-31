e39d111c72070fe2e8182746e5bfbb16c9b87f4a
Jose-Franco22 <Jose2005rfranco@gmail.com>
2025-03-30 21:07:45 -0500

    done

--- hwPQ/minpriorityqueue.h
+++ hwPQ/minpriorityqueue.h
@@ -1,6 +1,8 @@
+
 #ifndef MINPRIORITYQUEUE_H
 #define MINPRIORITYQUEUE_H
 
+// NOTE: You may not include any other libraries!
 #include <unordered_map>
 #include <vector>
 #include <utility> // Has pair and swap
@@ -10,126 +12,144 @@ using namespace std;
 template <typename T>
 class MinPriorityQueue
 {
-    public:
-        // Creates an empty MinPriorityQueue
+	// For the mandatory running times below:
+	//
+	// n is the number of elements in the MinPriorityQueue.
+	//
+	// Assume that the operations of unordered_map are O(1) time 
+	// (they are average case, but not worst-case).
+
+	public:
+		// Creates an empty MinPriorityQueue
         MinPriorityQueue()
         {
-            // No initialization needed as vectors and unordered_maps
-            // initialize themselves to empty
+            H.clear();
+            I.clear();
         }
 
-        // Returns the number of elements in the MinPriorityQueue.
+		// Returns the number of elements in the MinPriorityQueue.
+		//
+		// Must run in O(1) time.
         int size()
         {
             return H.size();
         }    
 
-        // Pushes a new value x with priority p into the MinPriorityQueue.
+
+		// Pushes a new value x with priority p
+		// into the MinPriorityQueue.
+		//
+		// Must run in O(log(n)) time.		
         void push(T x, int p)
         {
-            // Add to end of heap
             H.emplace_back(x, p);
-            // Record its index in the map
             I[x] = H.size() - 1;
-            // Bubble up to maintain heap property
-            bubble_up(H.size() - 1);
+            bubbleup(H.size() - 1);
         }
 
-        // Returns the value at the front of the MinPriorityQueue.
+		// Returns the value at the front of the MinPriorityQueue.
+		// Undefined behavior if the MinPriorityQueue is empty.
+		// 
+		// Must run in O(1) time.
         T front()
         {
             return H[0].first;
         }
 
         // Removes the value at the front of the MinPriorityQueue.
+		// Undefined behavior if the MinPriorityQueue is empty.
+		//
+		// Must run in O(log(n)) time. 
         void pop()
         {
-            if (H.empty()) return;
-            
-            // Remove from index map
+            if(H.empty()){
+                return;
+            }
+
             I.erase(H[0].first);
-            
-            if (H.size() > 1) {
-                // Move last element to root
+
+            if(H.size() > 1){
                 H[0] = H.back();
                 I[H[0].first] = 0;
             }
             
-            // Remove last element
             H.pop_back();
             
-            if (!H.empty()) {
-                // Bubble down the new root
-                bubble_down(0);
+            if(!H.empty()){
+                bubbledown(0);
             }
         }
 
-        // Changes the priority of x to new_p if current priority is at least new_p
+        // If x is in the MinPriorityQueue 
+		// with current priority at least new_p,  
+		// then changes the priority of x to new_p.
+		// Undefined behavior otherwise.
+		//
+		// Must run in O(log(n)) time. 
         void decrease_key(T x, int new_p)
         {
-            // Check if x exists and its current priority is >= new_p
-            if (I.find(x) == I.end() || H[I[x]].second <= new_p) {
-                return; // or we could throw an exception
+            if(H[I[x]].second <= new_p){
+                return; 
             }
-            
-            int index = I[x];
-            // Update the priority
-            H[index].second = new_p;
-            // Bubble up to maintain heap property
-            bubble_up(index);
+
+            int ind = I[x];
+            H[ind].second = new_p;
+            bubbleup(ind);
         }
 
     private:
         vector< pair<T, int> > H; // The heap.
         unordered_map<T, int> I;  // Maps values to their indices in H.
         
-        // Helper function to maintain heap property by bubbling up
-        void bubble_up(int index)
-        {
-            while (index > 0) {
-                int parent = (index - 1) / 2;
-                if (H[index].second >= H[parent].second) {
-                    break;
-                }
-                // Swap with parent
-                swap(H[index], H[parent]);
-                // Update indices in map
-                I[H[index].first] = index;
-                I[H[parent].first] = parent;
-                // Move up
-                index = parent;
+        int right_C(int i){
+            return 2 * i + 2;
+        }
+                
+        int left_C(int i){
+            return 2 * i + 1;
+        }
+                
+        int parent(int i){
+            return (i - 1) / 2;
+        }
+        
+        void swapper(int a, int b){
+            swap(H[a], H[b]);
+            I[H[a].first] = a;
+            I[H[b].first] = b;
+        }
+        
+        void bubbleup(int i){
+            if(i == 0){
+                return;
+            }
+
+            int par = parent(i);
+
+            if(H[i].second < H[par].second){
+                swapper(i, par);
+                bubbleup(par);
             }
         }
         
-        // Helper function to maintain heap property by bubbling down
-        void bubble_down(int index)
-        {
-            int left, right, smallest;
-            while (true) {
-                left = 2 * index + 1;
-                right = 2 * index + 2;
-                smallest = index;
-                
-                if (left < H.size() && H[left].second < H[smallest].second) {
-                    smallest = left;
-                }
-                if (right < H.size() && H[right].second < H[smallest].second) {
-                    smallest = right;
-                }
-                
-                if (smallest == index) {
-                    break;
-                }
-                
-                // Swap with smallest child
-                swap(H[index], H[smallest]);
-                // Update indices in map
-                I[H[index].first] = index;
-                I[H[smallest].first] = smallest;
-                // Move down
-                index = smallest;
+        void bubbledown(int i){
+            int left = left_C(i);
+            int right = right_C(i);
+            int smallest = i;
+
+            if (left < H.size() && H[left].second < H[smallest].second) {
+                smallest = left;
+            }
+            if (right < H.size() && H[right].second < H[smallest].second) {
+                smallest = right;
+            }
+
+            if (smallest != i){
+                swapper(i, smallest);
+                bubbledown(smallest);
             }
         }
-};
+
+        };
 
 #endif
